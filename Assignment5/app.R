f165dbf6df2ab3655e61d3ee7e04c87c72f97acd
Sarah Conway <sarahconway22@hotmail.com>
2025-11-24 16:12:12 +0000

    24/11/2025 Sarah and Judith created the GitHub for Assigment 5.They both outlined the aim for the project, sarah loaded in the required dataset, trying to make basic toggling tools for the app to adjust outputs.

--- Assignment5/app.R
+++ Assignment5/app.R
@@ -7,45 +7,87 @@
 #    https://shiny.posit.co/
 #
 
+if (!require("shiny")) install.packages("shiny")
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("DynNom")) install.packages("DynNom")
+if (!require("shinydashboard")) install.packages("shinydashboard")
+library(tidyverse)
 library(shiny)
+
+
+
+
+DIG_1 <- DIG_1 %>%
+  mutate(
+    TRTMT    = factor(TRTMT, levels = c(0, 1), labels = c("Placebo", "Treatment")),
+    SEX      = factor(SEX, levels = c(1, 2), labels = c("Male", "Female")),
+    HYPERTEN = factor(HYPERTEN, levels = c(0, 1), labels = c("No history", "History of hypertension")),
+    DIG      = factor(DIG, levels = c(0, 1), labels = c("No toxicity", "Digoxin toxicity")),
+    HOSP     = factor(HOSP, levels = c(0, 1), labels = c("No hospitalisation", "Hospitalisation")),
+    DEATH    = factor(DEATH, levels = c(0, 1), labels = c("Alive", "Death")),
+    CVD      = factor(CVD, levels = c(0, 1), labels = c("No CVD", "Cardiovascular disease")),
+    WHF      = factor(WHF, levels = c(0, 1), labels = c("No worsening HF", "Worsening Heart Failure")),
+    RACE     = factor(RACE, levels = c(1, 2), labels = c("White", "Nonwhite")
+  )
+  )
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("DIG Trial Analysis"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          selectInput(inputId = "RACE", label = "Select Patient Race:",
+                      choices = c("White", "Nonwhite"), multiple = FALSE),
+            radioButtons(inputId = "SEX", label = "Select Patient Gender:",
+                        choices = c("Male", "Female")),
+            radioButtons(inputId = "TRTMT", label = "Select Treatment Group:",
+                         choices = c("Placebo", "Treatment")),
+            sliderInput("AGE", "Select Patient Age Range:",
+                    min = 21, max = 90, value = c(30,60))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("plot1")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  patients_subset <- reactive({
+    DIG_1 %>%
+      filter(
+        SEX == input$SEX,
+        RACE == input$RACE,
+        AGE >= input$AGE[1],
+        AGE <= input$AGE[2],
+        TRTMT == input$TRTMT
+      )
+  })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$plot1 <- renderPlot({
+    ggplot(patients_subset(), aes(x = DIABP, y = SYSBP)) +
+      geom_point() +
+      labs(
+        title = "SYSBP vs DIABP (Filtered Patients)",
+        x = "Diastolic Blood Pressure",
+        y = "Systolic Blood Pressure"
+      ) +
+      theme_minimal()
+  })
+  
+
 }
+observe({
+  print(nrow(patients_subset()))
+})
 
-# Run the application 
-shinyApp(ui = ui, server = server)
